c1865b8898133fdd4eb1304934a486055b87c429
illided <utkin.ilya.nickolaevich@gmail.com>
2020-11-14 18:52:08 +0300

    now list of cats must be sent instead of raw ids

--- src/tables/BGMechanics.kt
+++ src/tables/BGMechanics.kt
@@ -6,17 +6,27 @@ import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 
 object BGMechanics : Table() {
-    val id: Column<Int> = integer("id")
+    val id: Column<String> = varchar("id", 3)
     val name: Column<String> = varchar("name", 50)
 
     override val primaryKey = PrimaryKey(id, name)
 
-    fun find(ids: List<Int>?): List<String> {
+    fun findById(ids: List<String>?): List<String> {
         if (ids == null) {
             return emptyList()
         }
         return transaction {
-            BGMechanics.select { BGMechanics.id inList ids}.map { it[name].toString() }
+            BGMechanics.select { BGMechanics.id inList ids }.map { it[name].toString() }
         }
+    }
+
+    fun findByName(names: List<String>?): List<String> {
+        if (names == null) {
+            return emptyList()
+        }
+
+        return transaction {
+            BGMechanics.select { name inList names }.map { it[BGMechanics.id] }
+        }.also { if (it.size != names.size) throw IllegalArgumentException("Not all mechanics were founded") }
     }
 }
