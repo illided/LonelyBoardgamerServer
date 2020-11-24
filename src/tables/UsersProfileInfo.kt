1263efaa53d9043ad136531e18995bd26344e2a2
illided <utkin.ilya.nickolaevich@gmail.com>
2020-11-24 14:20:25 +0300

    unnecessary functions removed

--- src/tables/UsersProfileInfo.kt
+++ src/tables/UsersProfileInfo.kt
@@ -3,8 +3,6 @@ package com.twoilya.lonelyboardgamer.tables
 import com.fasterxml.jackson.annotation.JsonInclude
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
-import org.jetbrains.exposed.sql.select
-import org.jetbrains.exposed.sql.transactions.transaction
 
 object UsersProfileInfo : Table() {
     private const val FIRST_NAME_MAX_LENGTH = 20
@@ -12,7 +10,6 @@ object UsersProfileInfo : Table() {
     private const val PREF_CATEGORIES_MAX_LENGTH = 50
     private const val PREF_MECHANICS_MAX_LENGTH = 50
     private const val DESCRIPTION_MAX_LENGTH = 250
-
 
     val id: Column<String> = varchar("id", 10)
     val firstName: Column<String> = varchar("firstName", FIRST_NAME_MAX_LENGTH)
@@ -25,33 +22,6 @@ object UsersProfileInfo : Table() {
     val prefMechanics: Column<String?> = varchar("prefMechanics", PREF_MECHANICS_MAX_LENGTH).nullable()
 
     override val primaryKey = PrimaryKey(id)
-
-    fun find(userId: String): ProfileInfo? {
-        val users = transaction {
-            UsersProfileInfo.select { UsersProfileInfo.id eq userId }
-                .limit(1)
-                .map { row ->
-                    ProfileInfo(
-                        id = row[UsersProfileInfo.id],
-
-                        firstName = row[firstName],
-                        secondName = row[secondName],
-
-                        address = row[address],
-
-                        description = row[description] ?: "",
-
-                        prefCategories = BGCategories.findById(row[prefCategories]?.split(",")),
-                        prefMechanics = BGMechanics.findById(row[prefMechanics]?.split(","))
-                    )
-                }
-        }
-        return if (users.isEmpty()) {
-            null
-        } else {
-            users[0]
-        }
-    }
 }
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -63,15 +33,4 @@ data class ProfileInfo (
     val prefCategories: List<String>,
     val prefMechanics: List<String>,
     val description: String
-) {
-    fun onlyPublic(): ProfileInfo {
-        return ProfileInfo(
-            id = id,
-            firstName = firstName,
-            secondName = secondName,
-            prefCategories = prefCategories,
-            prefMechanics = prefMechanics,
-            description = description
-        )
-    }
-}
+)
